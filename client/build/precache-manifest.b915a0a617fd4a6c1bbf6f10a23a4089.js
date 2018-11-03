5e3dc61f41668bd45aa5f53e43a8f72ac9dc3b19
Craig Melville <acekreations@gmail.com>
2018-11-02 20:06:12 -0700

    starting over

---
+++ client/build/precache-manifest.b915a0a617fd4a6c1bbf6f10a23a4089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a71f25e3498a3e04fa3f",
+    "url": "/static/js/main.a71f25e3.chunk.js"
+  },
+  {
+    "revision": "2e247460eb2ab4cb257a",
+    "url": "/static/js/1.2e247460.chunk.js"
+  },
+  {
+    "revision": "a71f25e3498a3e04fa3f",
+    "url": "/static/css/main.ab9bbfb4.chunk.css"
+  },
+  {
+    "revision": "9d5ecc9d46b070d3f76fd85272d65805",
+    "url": "/index.html"
+  }
+];
