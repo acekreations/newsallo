41cd542af58230e7dbd66aaca4600379160f4743
Craig Melville <acekreations@gmail.com>
2018-11-02 18:10:43 -0700

    changed static

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use(express.static("../client/public"));
-
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+
+app.use(express.static("../client/public"));
 
 app.listen(PORT, function() {
     console.log(`Listening on port ${PORT}`);
